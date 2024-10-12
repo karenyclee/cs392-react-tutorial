dca2dec709e70ee5e3fa3181b4179b5d8881efcc
Karen <karen040717@gmail.com>
2024-10-11 22:02:35 -0500

    implement selecting courses

--- src/components/Chooser.jsx
+++ src/components/Chooser.jsx
@@ -4,11 +4,17 @@ import CourseList from "./CourseList";
 
 const Chooser = ({courses}) => {
   const [choice, setChoice] = useState("Fall");
+  const [selected, setSelected] = useState([]);
 
+  const toggleSelected = (item) => setSelected(
+    selected.includes(item)
+    ? selected.filter(x => x !== item)
+    : [...selected, item]
+  );
   return (
     <>
       <SelectTerm selection={choice} setSelection={setChoice}/>
-      <CourseList courses={courses} term={choice}/>
+      <CourseList courses={courses} term={choice} selected={selected} toggleSelected={toggleSelected}/>
     </>
   );
 };
--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -2,20 +2,31 @@ import Course from "./Course"
 import "./CourseList.css"
 import "./SelectTerm"
 
-const CourseList = ({courses, term}) => {
-
-  const filtered = Object.entries(courses).filter(
-    ([id, courseinfo]) => courseinfo.term === term
-  );
-
-  return(
-    <div className="course-list">
-      {filtered.map(([id, courseinfo]) => 
-        <Course key={id} courseinfo={courseinfo} />
-      )}
-    </div>
-  );
+const CourseList = ({courses, term, selected, toggleSelected}) => {
+    const filtered = Object.entries(courses).filter(
+      ([id, course]) => course.term === term);
+    
+    return(
+      <div className="course">
+        {filtered.map(([id, courseinfo]) => (
+          <div 
+            key={id}
+            className={`course ${selected.includes(id) ? 'selected' : ''}`}
+            onClick={() => toggleSelected(id)}
+          >
+            <div className="course-body">
+              <h5>
+                {courseinfo.term} CS {courseinfo.number}
+              </h5>
+              <p>{courseinfo.title}</p>
+              <div className = "course-bottom">
+                <p>{courseinfo.meets}</p>
+              </div>
+              </div>
+          </div>
+        ))}
+      </div>
+    );
 };
-
 
 export default CourseList;
